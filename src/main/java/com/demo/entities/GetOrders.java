67847136b455bf3d9fb4b826634a15631632312d
chaosheng1991 <1145460677@qq.com>
2019-08-12 10:52:41 +0800

    waitOrders to radeOrders

--- src/main/java/com/demo/entities/GetOrders.java
+++ src/main/java/com/demo/entities/GetOrders.java
@@ -2,4 +2,8 @@ package com.demo.entities;
 
 public class GetOrders {
 
+    public void getOrder(){
+        //get orders from Orders table
+        
+    }
 }
--- src/main/java/com/demo/entities/Trade.java
+++ src/main/java/com/demo/entities/Trade.java
@@ -7,10 +7,26 @@ public class Trade {
     public static float buyCurrentPrice;
     public static float sellCurrentPrice;
     public static Queue tradeQueue=new ConcurrentLinkedQueue();
+    public static Queue waitQueue=new ConcurrentLinkedQueue();
 
-    public  synchronized String pollout(){
+    public  synchronized void offerTradeQueue(Trade trade){
+        tradeQueue.offer(trade);
+    }
+
+    public  synchronized Trade polloutTradeQueue(){
         if(!tradeQueue.isEmpty())
-            return tradeQueue.poll().toString();
+            return (Trade) tradeQueue.poll();
+        else
+            return null;
+    }
+
+    public  synchronized void offerWaitQueue(Trade trade){
+        waitQueue.offer(trade);
+    }
+
+    public  synchronized Trade polloutWaitQueue(){
+        if(!waitQueue.isEmpty())
+            return (Trade) waitQueue.poll();
         else
             return null;
     }
