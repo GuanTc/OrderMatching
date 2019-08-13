9f5a0867cd4b54625af6209a69922edcdd4e44d0
chaosheng1991 <1145460677@qq.com>
2019-08-13 11:55:50 +0800

    userController.java

--- src/main/java/com/demo/controller/UserController.java
+++ src/main/java/com/demo/controller/UserController.java
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-//package com.demo.controller;
-//
-//import com.demo.service.UserServiceImpl.UserSerciveImpl;
-//import com.demo.user.pojo.User;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//
-//import java.util.List;
-//
-///**
-// * @author: WANGSHUAIYI
-// * @date : 2019/8/12
-// * @description:
-// */
-//@Controller
-//@RequestMapping("/user")
-//public class UserController {
-//    @Autowired
-//    private UserSerciveImpl userSercive;
-//    @RequestMapping("/register")
-//    public String register(User user){
-//        userSercive.register(user);
-//        return "index";
-//    }
-//
-//    @RequestMapping("/findAll")
-//    @ResponseBody
-//    private List<User> findAll(){
-//        return userSercive.findAll();
-//    }
-//}
-=======
 package com.demo.controller;
 
 
@@ -123,4 +88,3 @@ public class UserController {
 
 
 }
->>>>>>> 84c747e2c45fa68a58af2ffd1d5b517fbba65a23
